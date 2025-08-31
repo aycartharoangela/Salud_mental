9f7cade8c21ec16004f20702319a0197fe5e251a
aycartharoangela <aycartharo.angela@gmail.com>
2025-08-31 19:53:21 +0200

    Data cleaning.R

--- Datos/Código depuración.R
+++ Datos/Código depuración.R
@@ -1,7 +1,14 @@
+
 # depuracion.R
 # Proyecto Salud Mental - CRONOS2 Wave 4
 
-library(dplyr)
-df <- read.csv("Datos/Base de datos original/CRON2W4.csv")
-head(df)
+library(readr)
+df <- read_csv("Datos/Base de datos original/CRON2W4e01.csv")
 
+df_clean <- df %>%
+  mutate(across(where(is.numeric), ~na_if(., 77))) %>%
+  mutate(across(where(is.numeric), ~na_if(., 88))) %>%
+  mutate(across(where(is.numeric), ~na_if(., 99)))
+
+# Guardar versión depurada
+write.csv(df_clean, "Datos/Base de datos depurada/CRON2W4_clean.csv", row.names = FALSE)
